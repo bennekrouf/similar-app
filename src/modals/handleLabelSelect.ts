ed8df7bb46dacc1d8b10cf82251aab1c6498ce4d
Mohamed Bennekrouf <mohamed.bennekrouf@gmail.com>
2023-09-28 09:04:01 +0200

    First refactor of sourate selection that almost works

--- src/modals/handleLabelSelect.ts
+++ src/modals/handleLabelSelect.ts
@@ -1,42 +1,75 @@
 import labels from './labels.json';
 
-export const handleLabelSelect = (selectedLabels, setSelectedLabels, labelName:string, start?:number, end?:number) => {
-    if (labelName === "حافظ") {
-        if (selectedLabels[labelName]) {
-            // If "حافظ" is already selected and we're clicking on it again, we should deselect all labels
-            setSelectedLabels({});
-        } else {
-            // If "حافظ" is not selected, select all labels
-            const allLabels = {};
-            [...labels.chapters, ...labels.otherDivisions, ...labels.quarters].forEach(label => {
-                allLabels[label.name] = true;
-            });
-            setSelectedLabels(allLabels);
-        }
-    } else if (labelName === "الربع الرابع") {
-      if (selectedLabels[labelName]) {
-          // If "الربع الرابع" is already selected and we're clicking on it again, we should deselect all related labels
-          const updatedSelection = { ...selectedLabels };
-          labels.otherDivisions.filter(division => division.start >= start && division.end <= end).forEach(label => {
-              updatedSelection[label.name] = false;
-          });
-          updatedSelection["الربع الرابع"] = false; // Also deselect "الربع الرابع" itself
-          setSelectedLabels(updatedSelection);
+export const handleLabelSelect = (selectedLabels, setSelectedLabels, labelName: string, start?: number, end?: number) => {
+  switch (labelName) {
+    case "حافظ":
+      handleHafizSelection(selectedLabels, setSelectedLabels);
+      break;
+    default:
+      if (isQuarterOrDivision(labelName)) {
+        handleDivisionSelection(selectedLabels, setSelectedLabels, labelName);
       } else {
-          // If "الربع الرابع" is not selected, select all labels that fall within its range
-          const inRangeLabels = labels.otherDivisions.filter(division => division.start >= start && division.end <= end).map(label => label.name);
-          const updatedSelection = { ...selectedLabels };
-          inRangeLabels.forEach(label => {
-              updatedSelection[label] = true;
-          });
-          updatedSelection["الربع الرابع"] = true; // Also select "الربع الرابع" itself
-          setSelectedLabels(updatedSelection);
+        toggleLabelSelection(setSelectedLabels, labelName);
       }
-  } else {
-      // Toggle the current label
-      setSelectedLabels(prev => ({
-          ...prev,
-          [labelName]: !prev[labelName]
-      }));
+      break;
   }
-}
+};
+
+const handleHafizSelection = (selectedLabels, setSelectedLabels) => {
+  if (selectedLabels["حافظ"]) {
+    setSelectedLabels({});
+  } else {
+    const allLabels = {};
+    [...labels.chapters, ...labels.otherDivisions, ...labels.quarters].forEach(label => {
+      allLabels[label.name] = true;
+    });
+    setSelectedLabels(allLabels);
+  }
+};
+
+const handleDivisionSelection = (selectedLabels, setSelectedLabels, divisionName: string) => {
+    const division = labels.quarters.find(item => item.name === divisionName) || labels.otherDivisions.find(item => item.name === divisionName);
+    if (!division) return;
+  
+    const isDivisionSelected = selectedLabels[divisionName];
+    const updateLabelStatus = (labelName, status) => {
+      updatedSelection[labelName] = status;
+    };
+  
+    const updatedSelection = { ...selectedLabels };
+    processDivisionRelatedLabels(division, isDivisionSelected, updateLabelStatus);
+    setSelectedLabels(updatedSelection);
+  };
+
+  const processDivisionRelatedLabels = (division, isDivisionSelected, updateLabelStatus) => {
+    // Update status for division
+    updateLabelStatus(division.name, !isDivisionSelected);
+  
+    // Update status for related chapters
+    labels.chapters.filter(chapter => chapter.number >= division.start && chapter.number <= division.end).forEach(chapter => {
+      updateLabelStatus(chapter.name, !isDivisionSelected);
+    });
+  
+    // Update status for overlapping divisions
+    labels.otherDivisions.filter(divisionItem => 
+      isOverlapping(divisionItem.start, divisionItem.end, division.start, division.end)).forEach(divisionItem => {
+      updateLabelStatus(divisionItem.name, !isDivisionSelected);
+    });
+  };
+  
+  
+
+const toggleLabelSelection = (setSelectedLabels, labelName: string) => {
+  setSelectedLabels(prev => ({
+    ...prev,
+    [labelName]: !prev[labelName]
+  }));
+};
+
+const isQuarterOrDivision = (labelName: string) => {
+  return labels.quarters.some(quarter => quarter.name === labelName) || labels.otherDivisions.some(division => division.name === labelName);
+};
+
+const isOverlapping = (start1, end1, start2, end2) => {
+    return start1 <= end2 && end1 >= start2;
+  };
