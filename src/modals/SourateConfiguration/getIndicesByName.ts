0f5165251898c456af1316fcfae950e73cc76d29
Mohamed Bennekrouf <mohamed.bennekrouf@gmail.com>
2023-11-12 14:13:34 +0100

    Change indice by name fn to have distinct res

--- src/modals/SourateConfiguration/getIndicesByName.ts
+++ src/modals/SourateConfiguration/getIndicesByName.ts
@@ -10,19 +10,25 @@ type Entry = {
 const entries: Entry[] = labels as Entry[];
 
 export function getIndicesByName(names: string[]): number[] {
-    const ranges = entries
-      .filter(entry => names.includes(entry.name))
-      .map(entry => {
-        const start = entry.start;
-        const end = entry.end;
-        return Array.from({ length: end - start + 1 }, (_, i) => start + i);
-      });
-    
-    // Flatten the array of arrays into a single array and sort numerically
-    return ([] as number[]).concat(...ranges).sort((a, b) => a - b);
+  const ranges = entries
+    .filter(entry => names?.includes(entry.name))
+    .map(entry => {
+      const start = entry.start;
+      const end = entry.end;
+      return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+    });
+
+  // Flatten the array of arrays into a single array
+  const flattenedArray = ([] as number[]).concat(...ranges);
+
+  // Create a Set to remove duplicates and then convert it back to an array
+  const distinctIndices = Array.from(new Set(flattenedArray));
+
+  // Sort numerically
+  return distinctIndices.sort((a, b) => a - b);
 }
   
-  // Example usage:
+// Example usage:
 //   const namesToSearch = ["الربع الأول"];
 //   const indices = getIndicesByName(namesToSearch);
 //   console.log(indices); // Should log: [1, 2, 3, 4, 5, 6]
